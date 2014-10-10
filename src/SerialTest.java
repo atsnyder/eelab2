c935db635041ff42f2a0d3d5d2ccbead7ca01b5e
atsnyder <adamthomassnyder@gmail.com>
2014-10-09 20:06:08 -0500

    linux

--- src/SerialTest.java
+++ src/SerialTest.java
@@ -17,7 +17,7 @@ public class SerialTest implements SerialPortEventListener {
 	private static final String PORT_NAMES[] = { 
 			"/dev/tty.usbserial-A9007UX1", // Mac OS X
                         "/dev/ttyACM0", // Raspberry Pi
-			"/dev/ttyUSB0", // Linux
+			"/dev/ttyACM0", // Linux
 			"COM18", // Windows
 	};
 	/**
@@ -38,7 +38,7 @@ public class SerialTest implements SerialPortEventListener {
 	public void initialize() {
                 // the next line is for Raspberry Pi and 
                 // gets us into the while loop and was suggested here was suggested http://www.raspberrypi.org/phpBB3/viewtopic.php?f=81&t=32186
-                System.setProperty("gnu.io.rxtx.SerialPorts", "COM18");
+                System.setProperty("gnu.io.rxtx.SerialPorts", "/dev/ttyACM0");
 
 		CommPortIdentifier portId = null;
 		Enumeration portEnum = CommPortIdentifier.getPortIdentifiers();
